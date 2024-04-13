7f2178a9a62c8e0a892c44f936ad16a9929ac73e
guasonsad <guasonsad@GUASON-KING>
2024-04-13 02:10:01 -0500

    Clase Book NUEVO

--- Demo_02-main/MyLibrary/Book.cs
+++ Demo_02-main/MyLibrary/Book.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,4 +22,4 @@ namespace MyLibrary
         public string Title => Title;
 
     }
-}
+}*/
